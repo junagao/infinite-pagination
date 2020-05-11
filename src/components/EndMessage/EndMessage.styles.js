412f40e1a0ad3ee6f9b26843f06f0d9a4397e059
Juliane Nagao <julianens@gmail.com>
2020-05-11 14:35:37 +0200

    refactor: EndMessage.styles

--- src/components/EndMessage/EndMessage.styles.js
+++ src/components/EndMessage/EndMessage.styles.js
@@ -2,7 +2,7 @@ import styled from '@emotion/styled'
 
 const Text = styled.p`
   font-size: ${({ theme }) => theme.fontSizes.md};
-  color: ${(props) => props.theme.colors.text.body};
+  color: ${({ theme }) => theme.colors.text.body};
   padding: ${({ theme }) => `${theme.space[6]} ${theme.space[0]}`};
 `
 
