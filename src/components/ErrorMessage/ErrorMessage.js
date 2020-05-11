952b0c88532a308a4a604c78b23e579d99f5295a
Juliane Nagao <julianens@gmail.com>
2020-05-11 14:35:13 +0200

    feat: test ErrorMessage

--- src/components/ErrorMessage/ErrorMessage.js
+++ src/components/ErrorMessage/ErrorMessage.js
@@ -3,7 +3,11 @@ import { string } from 'prop-types'
 import Text from './ErrorMessage.styles'
 
 export default function ErrorMessage({ text }) {
-  return <Text>The following error occurred: {text}. Please try again.</Text>
+  return (
+    <Text data-testid="error-message">
+      The following error occurred: {text}. Please try again.
+    </Text>
+  )
 }
 
 ErrorMessage.propTypes = {
